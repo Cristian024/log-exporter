d719c1d5e3f94ce2170b628b5284a4255a396255
CristianRobledo0705 <cristiandavidrobledoleo@gmail.com>
2024-07-19 01:25:56 -0500

    feat: logic for logs storage has updated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-export function test(){
-    console.log("Test de funcionamiento");
-}
+(async function () {
+    const originalLog = console.log;
+    console.log = function (...args) {
+        customLogCallback(args);
+        originalLog.apply(console, args);
+    };
+    console.test = function (...args){
+        originalLog.apply(console, args);
+    }
+
+    function customLogCallback(logArgs) {
+        const logDay = new Date();
+        const logName = `user.log-${logDay.getFullYear()}${logDay.getMonth()}${logDay.getDay()}`;
+
+        const log = JSON.parse(localStorage.getItem(logName)) || [];
+
+        const objectLog = {
+            date: `${logDay.getFullYear()}-${logDay.getMonth()}-${logDay.getDay()} ${logDay.getHours()}:${logDay.getMinutes()}:${logDay.getSeconds()}.${logDay.getMilliseconds()}`,
+            message: logArgs[0]
+        }
+
+        log.push(objectLog);
+        localStorage.setItem(logName, JSON.stringify(log))
+    }
+})();
+
